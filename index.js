957a51aa437d1442599d286b186c347ad1e6552a
dodsonta <tdodson@pdx.edu>
2026-03-12 18:03:05 -0700

    Added the actual carousel part of the caraousel (created javascript and css to make it act like one)

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ window.onbeforeunload = () => {
     form.reset();
   }
 };
+
+let slideIdx = 1;
+showSlides(slideIdx);
+
+function changeSlide(n) {
+  showSlides((slideIdx += n));
+}
+
+function currSlide(n) {
+  showSlides((slideIdx = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+    slideIdx = 1;
+  }
+  if (n < 1) {
+    slideIdx = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIdx - 1].style.display = "block";
+}
